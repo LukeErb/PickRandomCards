ee2c42c629b8a8e722fc131b4a349531f74b8b21
19004473CTC <19004473@student.muskegonisd.org>
2023-11-03 14:00:41 -0400

    Added card counter and spaces

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -14,22 +14,23 @@ namespace PickRandomCards
             string Hands = Console.ReadLine();
             Console.Write("Enter the number of cards per hand: ");
             string Cards = Console.ReadLine();
-            if (int.TryParse(Hands, out int numberOfHands) && int.TryParse(Cards, out int numberOfCards) && numberOfCards <= 52 && numberOfCards > 0 && numberOfHands > 0 )
+            if (int.TryParse(Hands, out int numberOfHands) && int.TryParse(Cards, out int numberOfCards) && numberOfCards <= 52 && numberOfCards > 0 && numberOfHands > 0)
             {
+                int totalCards = numberOfHands * numberOfCards;
                 while (numberOfHands > 0)
                 {
                     foreach (string card in CardPicker.PickSomeCards(numberOfCards))
                     {
-                        Console.WriteLine(card);
+                        Console.WriteLine($"\n {card}");
                     }
                     numberOfHands--;
                 }
+                Console.WriteLine($"\nTotal cards: {totalCards}");
             }
             else
             {
                 Console.WriteLine("You can only enter valid numbers");
             }
-
         }
     }
 }
